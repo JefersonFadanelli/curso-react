f9f812cbe46b8c0cbeadaef2621abbbfbc45c790
Jeferson Fadanelli <jeffefadanelli@gmail.com>
2020-04-17 10:14:23 -0300

    Renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
-import Membro from './Components/Membro';
-
 export default class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+          show: false  
+        };
+
+        this.sair = this.sair.bind(this);
+        this.entrar = this.entrar.bind(this);
+    }
+
+    sair(){
+        this.setState({show: false});
+    }
+
+    entrar(){
+        this.setState({show: true});
+    }
 
     render(){
         return (
             <div>
-                <Membro nome="Jef"/>
+
+                { this.state.show ?
+                    <div> 
+                        <h1>bem vindo ao sistema</h1>
+                        <button onClick={this.sair}> Sair </button>
+                    </div>
+                    :
+                    <div> 
+                        <h1>Acesse o sistema </h1>
+                        <button onClick={this.entrar}> Acessar </button>
+                    </div>
+                }
+                
             </div>
         );
     }
